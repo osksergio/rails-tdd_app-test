febcf7a26cffc9f8c46fb8c3a4a2bb1ea6b609d7
Sergio Oseko <osk.sergio@gmail.com>
2023-09-10 10:01:13 -0300

    build: utilizando a gem faker na fábrica (fim da aula #51)

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Customer, type: :model do
 
   it 'create a customer' do
     customer = create(:customer)
-    expect(customer.full_name).to eq('Sr. Sergio Oseko')
+    expect(customer.full_name).to start_with("Sr. ")
   end
+
+  it { expect{ create(:customer) }.to change{ Customer.all.size }.by(1) }
 end
