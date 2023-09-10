77af83c0c5c19530091559989d1b17167552af0d
Sergio Oseko <osk.sergio@gmail.com>
2023-09-10 09:25:38 -0300

    feat: utilizando a factory nos testes - fim da aula #50

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  fixtures :customers
 
   it 'create a customer' do
-    customer = customers(:sergio)
+    customer = create(:customer)
     expect(customer.full_name).to eq('Sr. Sergio Oseko')
   end
 end
