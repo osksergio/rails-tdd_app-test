82cc6a329777b5d5092d40c241522ef7aaaf899a
Sergio Oseko <osk.sergio@gmail.com>
2023-09-10 19:31:18 -0300

    test: sobrescrevendo atributos e usando aliases (factory)

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -7,5 +7,15 @@ RSpec.describe Customer, type: :model do
     expect(customer.full_name).to start_with("Sr. ")
   end
 
+  it 'create a customer (usando um apelido para a factory)' do
+    customer = create(:user)
+    expect(customer.full_name).to start_with("Sr. ")
+  end
+
+  it 'create a customer (sobrescrevendo um atributo da factory)' do
+    customer = create(:customer, name: 'Sergio Oseko')
+    expect(customer.full_name).to eq('Sr. Sergio Oseko')
+  end
+
   it { expect{ create(:customer) }.to change{ Customer.all.size }.by(1) }
 end
