617176d63e578674dc5d4441028a3bd6e294a44f
Sergio Oseko <osk.sergio@gmail.com>
2023-09-17 08:02:02 -0300

    feat: dois novos campos

--- spec/factories/customer.rb
+++ spec/factories/customer.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :customer, aliases: [:user] do
     name { Faker::Name.name }
     email { Faker::Internet.email }
+    vip { true }
+    days_to_pay { 30 }
   end
 end
