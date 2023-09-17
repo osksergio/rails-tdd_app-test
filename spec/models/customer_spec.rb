6d211208af5f9a7647f9c73378ee5aa593b2fe3e
Sergio Oseko <osk.sergio@gmail.com>
2023-09-17 08:26:31 -0300

    test: herança (factory)

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,19 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
 
-  it 'create a customer' do
+  it '# create a customer' do
     customer = create(:customer)
     expect(customer.full_name).to start_with("Sr. ")
   end
 
-  it 'create a customer (usando um apelido para a factory)' do
+  it '# create a customer (usando um apelido para a factory)' do
     customer = create(:user)
     expect(customer.full_name).to start_with("Sr. ")
   end
 
-  it 'create a customer (sobrescrevendo um atributo da factory)' do
+  it '# create a customer (sobrescrevendo um atributo da factory)' do
     customer = create(:customer, name: 'Sergio Oseko')
     expect(customer.full_name).to eq('Sr. Sergio Oseko')
+  end
+
+  it '# usando a herança das factories' do
+    customer = create(:customer_vip)
+    expect(customer.vip).to eq(true)
+  end
+
+  it '# mais um exemplo de herança das factories' do
+    customer = create(:customer_default)
+    expect(customer.vip).to eq(false)
   end
 
   it { expect{ create(:customer) }.to change{ Customer.all.size }.by(1) }
