4d4525a6df28167a3f8ce15a6d26c54b73a5d060
Sergio Oseko <osk.sergio@gmail.com>
2023-09-27 22:28:43 -0300

    feat: novo campo address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_20_085935) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_28_012403) do
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_085935) do
     t.integer "days_to_pay"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
   create_table "orders", force: :cascade do |t|
