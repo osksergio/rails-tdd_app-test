349d1678088c66758d2b844134490757bd46aaa8
Sergio Oseko <osk.sergio@gmail.com>
2023-10-12 11:17:33 -0300

    test: aula #68 (VCR c/ metadados do RSpec)

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,12 +1,19 @@
 describe 'httparty' do
-  it 'httparty' do
+  it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts' } do
     #stub_request(:get, "https://jsonplaceholder.typicode.com/posts/1").
     #  to_return(status: 200, body: "", headers: { 'content-type': 'application/json' })
 
-    VCR.use_cassette("jsonplaceholder/posts") do
-      response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/1')
-      content_type = response.headers['content-type']
-      expect(content_type).to match(/application\/json/)      
-    end
+    # primeira versão do teste usando a gem vcr
+    # ainda não estava configurado vcr com metadados do RSpec
+    # -------------------------------------------------------
+    #VCR.use_cassette("jsonplaceholder/posts") do
+    #  response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/1')
+    #  content_type = response.headers['content-type']
+    #  expect(content_type).to match(/application\/json/)
+    #end
+
+    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/1')
+    content_type = response.headers['content-type']
+    expect(content_type).to match(/application\/json/)
   end
 end
