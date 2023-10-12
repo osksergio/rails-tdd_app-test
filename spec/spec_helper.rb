03e1beff32bd982dd4638b48e7cd21d231f8927a
Sergio Oseko <osk.sergio@gmail.com>
2023-10-12 10:04:12 -0300

    test: fim da aula #67 (VCR)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'httparty'
 require 'vcr'
 
 VCR.configure do |config|
-  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
 end
 
