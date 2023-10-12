98c3e9d9b7ad7e17f20562c37872434389fc149c
Sergio Oseko <osk.sergio@gmail.com>
2023-10-12 09:11:10 -0300

    build: config gem vcr

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,7 +1,7 @@
 describe 'httparty' do
   it 'httparty' do
-    stub_request(:get, "https://jsonplaceholder.typicode.com/posts/1").
-      to_return(status: 200, body: "", headers: { 'content-type': 'application/json' })
+    #stub_request(:get, "https://jsonplaceholder.typicode.com/posts/1").
+    #  to_return(status: 200, body: "", headers: { 'content-type': 'application/json' })
 
     response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/1')
         content_type = response.headers['content-type']
