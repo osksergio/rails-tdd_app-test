561ac38963670e3dee5bb0a06cf31e5089fd9381
Sergio Oseko <osk.sergio@gmail.com>
2023-10-14 18:56:20 -0300

    test: aula #70 (VCR: URIs não determinísticas)

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,5 +1,5 @@
 describe 'httparty' do
-  it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts' } do
+  it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body] } do
     #stub_request(:get, "https://jsonplaceholder.typicode.com/posts/1").
     #  to_return(status: 200, body: "", headers: { 'content-type': 'application/json' })
 
@@ -12,7 +12,8 @@ describe 'httparty' do
     #  expect(content_type).to match(/application\/json/)
     #end
 
-    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/1')
+    #response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/1')
+    response = HTTParty.get("https://jsonplaceholder.typicode.com/posts/#{[1,2,3,4,5].sample}")
     content_type = response.headers['content-type']
     expect(content_type).to match(/application\/json/)
   end
