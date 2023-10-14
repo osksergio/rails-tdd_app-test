04dfced8356d9b00c6dd949d40de4162cf0ca362
Sergio Oseko <osk.sergio@gmail.com>
2023-10-14 19:18:03 -0300

    test: aula #71 (VCR: modos de gravação)

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,5 +1,6 @@
 describe 'httparty' do
-  it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body] } do
+  #it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body] } do
+  it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts', :record => :new_episodes } do
     #stub_request(:get, "https://jsonplaceholder.typicode.com/posts/1").
     #  to_return(status: 200, body: "", headers: { 'content-type': 'application/json'
 
@@ -13,7 +14,7 @@ describe 'httparty' do
     #end
 
     #response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/1')
-    response = HTTParty.get("https://jsonplaceholder.typicode.com/posts/#{[1,2,3,4,5].sample}")
+    response = HTTParty.get("https://jsonplaceholder.typicode.com/posts/4")
     content_type = response.headers['content-type']
     expect(content_type).to match(/application\/json/)
   end
