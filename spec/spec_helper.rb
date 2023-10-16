5d30f30595440cdedde08b96880d2da7b0a11fa8
Sergio Oseko <osk.sergio@gmail.com>
2023-10-15 21:18:17 -0300

    test: executar testes em ordem aleatória (aula #73)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,6 +26,7 @@ end
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.order = "random"
   # FactoryBot Lint
   #config.before(:suite) do
   #  FactoryBot.lint
