5b95b568793a0624341cc9606d53a4ff8c3d18e5
Sergio Oseko <osk.sergio@gmail.com>
2023-10-19 22:22:01 -0300

    test: fim da aula #76 (shoulda matchers)

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+  # ================================================================[ testes iniciais ]
   it 'is valid with description, price and category' do
     product = create(:product)
     expect(product).to be_valid
@@ -29,5 +30,16 @@ RSpec.describe Product, type: :model do
   it 'return a product with a full description' do
     product = create(:product)
     expect(product.full_description).to eq("#{product.description} - #{product.price}")
+  end
+
+  # ================================================= [ testes usando o shoulda matchers ]
+  context 'Validates' do
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:price) }
+    it { is_expected.to validate_presence_of(:category) }
+  end
+
+  context 'Associations' do
+    it { is_expected.to belong_to(:category) }
   end
 end
