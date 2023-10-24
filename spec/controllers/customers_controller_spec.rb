54b1df2312932c17551ade4b340ef6d0b3735852
Sergio Oseko <osk.sergio@gmail.com>
2023-10-24 06:14:37 -0300

    test: sem autenticação acessando o método show

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -10,4 +10,10 @@ RSpec.describe CustomersController, type: :controller do
     get :index
     expect(response).to have_http_status(200)
   end
+
+  it 'responds a 302 response (not authorized)' do
+    customer = create(:customer)
+    get :show, params: { id: customer.id }
+    expect(response).to have_http_status(302)
+  end
 end
