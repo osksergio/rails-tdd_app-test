d9bc99d56db070725659d2b692dffc15863127d0
Sergio Oseko <osk.sergio@gmail.com>
2023-10-23 22:33:29 -0300

    build: finalizando a instalação e configuração devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :members
   resources :customers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "home#index"
+  # root to: "home#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_16_181249) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_24_011916) do
   create_table "categories", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", null: false
@@ -25,6 +25,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_16_181249) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
+  end
+
+  create_table "members", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_members_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
 
   create_table "orders", force: :cascade do |t|
