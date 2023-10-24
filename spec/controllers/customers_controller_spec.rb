86a7e2f67f54cecf2d8f33d1dd278219f14dab58
Sergio Oseko <osk.sergio@gmail.com>
2023-10-24 06:28:08 -0300

    test: aula #79 (testando controllers - gem devise

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -1,19 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe CustomersController, type: :controller do
-  it 'response successfully' do
-    get :index
-    expect(response).to be_successful
+
+  context 'as a Guest' do
+    it 'response successfully' do
+      get :index
+      expect(response).to be_successful
+    end
+
+    it 'responds a 200 response' do
+      get :index
+      expect(response).to have_http_status(200)
+    end
+
+    it 'responds a 302 response (not authorized)' do
+      customer = create(:customer)
+      get :show, params: { id: customer.id }
+      expect(response).to have_http_status(302)
+    end
   end
 
-  it 'responds a 200 response' do
-    get :index
-    expect(response).to have_http_status(200)
-  end
-
-  it 'responds a 302 response (not authorized)' do
-    customer = create(:customer)
-    get :show, params: { id: customer.id }
-    expect(response).to have_http_status(302)
+  context 'as Logged Member' do
+    it "#show method" do
+      member = create(:member)
+      customer = create(:customer)
+      sign_in member
+      get :show, params: { id: customer.id }
+      expect(response).to have_http_status(200)
+    end
   end
 end
