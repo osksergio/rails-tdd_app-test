3e3ae96d4af822520d36dc83a0abf6d7e849b327
Sergio Oseko <osk.sergio@gmail.com>
2023-11-11 18:20:06 -0300

    test: be_a_kind_of e expressões regulares

--- spec/requests/customers_spec.rb
+++ spec/requests/customers_spec.rb
@@ -22,5 +22,25 @@ RSpec.describe "Customers", type: :request do
       expect(response).to have_http_status(200)
       expect(response.body).to include_json(id: 1)
     end
+
+    it "uses be_kind_of in the include_json" do
+      get "/customers.json"
+      expect(response).to have_http_status(200)
+      expect(response.body).to include_json([
+                                 id: /\d/,
+                                 name: (be_a_kind_of String),
+                                 email: (be_a_kind_of String),
+                                            ])
+    end
+
+    it "uses be_kind_of in the include_json (id = 1)" do
+      get "/customers/1.json"
+      expect(response).to have_http_status(200)
+      expect(response.body).to include_json(
+        id: /\d/,  # expressão regular indicando um número
+        name: (be_a_kind_of String),
+        email: (be_a_kind_of String),
+      )
+    end
   end
 end
