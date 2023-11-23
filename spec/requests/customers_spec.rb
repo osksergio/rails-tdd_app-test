ea427cb1edc997e8ec181a4db7e1fe95308f5bd8
Sergio Oseko <osk.sergio@gmail.com>
2023-11-22 22:02:07 -0300

    test: fim da aula #97 (delete)

--- spec/requests/customers_spec.rb
+++ spec/requests/customers_spec.rb
@@ -76,5 +76,15 @@ RSpec.describe "Customers", type: :request do
         email: customers.email
       )
     end
+
+    it 'destroy - JSON' do
+      member = create(:member)
+      login_as(member, scope: :member)
+
+      headers = { "ACCEPT" => "application/json" }
+      customers = Customer.first
+      except{ delete "/customers/#{customers.id}.json", headers: headers }.to change(Customer, :count).by(-1)
+      expect(response).to have_http_status(204)
+    end
   end
 end
